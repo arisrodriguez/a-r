c51b061e4ec9bea624c0b25931b1cb398318d29c
Aristides Rodríguez <12479256+arisrodriguez@users.noreply.github.com>
2025-05-20 00:02:45 -0500

    refactor(home): optimiza animación del h1 para mejorar el Largest Contentful Paint (LCP)

--- src/app/components/Home/HomeHeader.tsx
+++ src/app/components/Home/HomeHeader.tsx
@@ -8,20 +8,18 @@ export default function HomeHeader() {
             aria-labelledby="presentacion-heading"
             className="flex flex-col items-center text-center px-4"
         >
-            <motion.h1
-                initial={{ y: 8 }}
-                animate={{ y: 0 }}
-                transition={{ delay: 0.1, duration: 0.35, ease: "easeInOut" }}
+            {/* H1 optimizado para ser LCP */}
+            <h1
                 className="font-bold text-neutral-800 text-4xl"
                 id="presentacion-heading"
             >
                 ¡Hola, soy Aristides!
-            </motion.h1>
+            </h1>
 
             <motion.p
                 initial={{ opacity: 0, y: 10 }}
                 animate={{ opacity: 1, y: 0 }}
-                transition={{ delay: 0.2, duration: 0.35, ease: "easeInOut" }}
+                transition={{ delay: 0.3, duration: 0.35, ease: "easeInOut" }}
                 className="font-semibold text-gray-500 text-2xl mt-3"
             >
                 A veces construyo cosas.
@@ -30,7 +28,7 @@ export default function HomeHeader() {
             <motion.p
                 initial={{ opacity: 0, y: 10 }}
                 animate={{ opacity: 1, y: 0 }}
-                transition={{ delay: 0.3, duration: 0.35, ease: "easeInOut" }}
+                transition={{ delay: 0.4, duration: 0.35, ease: "easeInOut" }}
                 className="font-normal text-gray-500 text-base mt-6 max-w-2xl"
             >
                 Soy desarrollador de software en Panamá, especializado en la web con un enfoque centrado en el diseño. Además, practico atletismo y creo videos para Youtube.
@@ -39,7 +37,7 @@ export default function HomeHeader() {
             <motion.p
                 initial={{ opacity: 0, y: 10 }}
                 animate={{ opacity: 1, y: 0 }}
-                transition={{ delay: 0.4, duration: 0.35, ease: "easeInOut" }}
+                transition={{ delay: 0.5, duration: 0.35, ease: "easeInOut" }}
                 className="font-normal text-gray-500 text-base mt-2 max-w-2xl"
             >
                 Últimamente he estado trabajando con tecnologías como{" "}
