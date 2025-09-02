2279469cae14e193377f10d4c0114758c7e8fea9
Aristides Rodríguez <12479256+arisrodriguez@users.noreply.github.com>
2025-09-02 00:28:12 -0500

    cambio en los estilos para light mode

--- src/app/components/About/AboutSocialLinks.tsx
+++ src/app/components/About/AboutSocialLinks.tsx
@@ -28,7 +28,7 @@ export default function AboutSocialLinks() {
                     <a
                         key={label}
                         href={href}
-                        className="h-12 rounded-lg flex items-center justify-between px-4 bg-neutral-900 text-gray-50 hover:bg-neutral-800 transition-all duration-300 ease-in-out dark:text-gray-50 dark:bg-neutral-800 dark:hover:bg-neutral-700"
+                        className="h-12 rounded-lg flex items-center justify-between px-4 bg-gray-100 text-neutral-900 hover:bg-gray-200 transition-all duration-300 ease-in-out dark:text-gray-50 dark:bg-neutral-800 dark:hover:bg-neutral-700"
                         target="_blank"
                         rel="noopener noreferrer"
                         aria-label={label}
